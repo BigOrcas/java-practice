d3800e70be3abf3594b0a59bc6fa900610e206bd
bigwhale1948@gmail.com <bigwhale1948@gmail.com>
2023-01-02 17:04:47 +0900

    [230102 SIP와 DIP]

--- src/main/java/ch05/OOPEx03.java
+++ src/main/java/ch05/OOPEx03.java
@@ -4,7 +4,7 @@ class 요리사{
     String name="요리사";
 }
 // 홍길동 or 요리사
-class 홍길동 extends 요리사{
+class 홍길동 extends ch05.sipdip.요리사 {
     String name="홍길동";
 }
 public class OOPEx03 {
@@ -12,7 +12,7 @@ public class OOPEx03 {
         홍길동 h1 = new 홍길동(); // 힙메모리에는 홍길동 ,요리사가 올라감
         System.out.println(h1.name);
 
-        요리사 y1 = new 홍길동(); // 힙메모리에는 홍길동 ,요리사가 올라감
+        ch05.sipdip.요리사 y1 = new 홍길동(); // 힙메모리에는 홍길동 ,요리사가 올라감
         System.out.println(y1.name);
 
         // 홍길동 h2 = new 요리사(); 불가능하다, 요리사를 바라보고있기때문
